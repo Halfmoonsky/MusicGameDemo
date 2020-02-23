11503f515228e4e3bc0b64a86fabc57269e03819
Halfmoonsky <33171865@qq.com>
2020-02-24 02:08:14 +0800

    titlefinish

---
+++ Halfmoon_Shooter/Assets/_Complete-Game/Scripts/Managers/SceneChange1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneSwitch();
+        }
+    }
+
+    private void SceneSwitch()
+    {
+        StartCoroutine(Load());
+    }
+    IEnumerator Load()
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync("ChooseSong");
+        yield return new WaitForEndOfFrame();
+        ao.allowSceneActivation = true;
+    }
+}
